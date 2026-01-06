a88308467a6fd1d993d19cea419b4940e79d1851
akavinprimepartners <akavin.prime.partners@gmail.com>
2026-01-07 00:36:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple welcome message
+console.log("Welcome to Akavin Website");
+
+// Example: Smooth scroll for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
